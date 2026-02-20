4aba949de076cc8ca0818bbe61943c2dd404fb39
Student <Student@PGEESTEM-34>
2026-02-20 11:33:39 +0200

    Add project files.

---
+++ cows and bulls/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using cows_and_bulls.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cows_and_bulls.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ cows and bulls/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cows_and_bulls.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
