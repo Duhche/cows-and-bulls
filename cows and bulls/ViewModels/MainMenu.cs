f661b1ac2378d32a9f63efdcb502bb584180a809
Duhche <duhche24@gmail.com>
2026-03-03 17:04:13 +0200

    main menu napraveno no gurmi zashtoto nyama controller controller manyaka da go navurje

---
+++ cows and bulls/ViewModels/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿namespace cows_and_bulls.ViewModels
+{
+    public class MainMenu
+    {
+        public string Title { get; set; }
+
+        public List<string> Options { get; set; }
+
+        public MainMenu()
+        {
+            Title = "Bletchley Code Breakers";
+
+            Options = new List<string>
+            {
+                "Player vs Player (No repeats)",
+                "Player vs Player (Repeats)",
+                "Player vs Computer (No repeats)",
+                "Player vs Computer (Repeats)",
+                "Exit"
+            };
+        }
+    }
+}
