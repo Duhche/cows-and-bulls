7a9826578fbc70b61f610451d4cbee8dc866d8ed
1 <1>
2026-03-04 12:14:56 +0200

    Models-add player table and add game table

---
+++ cows and bulls/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cows_and_bulls.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        public int Move { get; set; }
+
+        [Required]
+        public bool Win { get; set; }
+
+      
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+    }
+}
---
+++ cows and bulls/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cows_and_bulls.Models
+{
+    public class Player
+    {
+        
+            public int Id { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            public string Nickname { get; set; }
+
+            [Required]
+            [StringLength(255)]
+            public string Password { get; set; }
+        
+    }
+}
